de160be1cfcd78a6933f37378c34f8e610e2320f
Song0-0 <ks08094@naver.com>
2024-01-04 15:43:56 +0900

    h2db,라이브러리 셋팅, persistence.xml설정, EntityManagerFactory, EntityManager통해 작업, Transaction

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,17 +2,38 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        EntityManager entityManager = emf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
 
-        
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
-        entityManager.close();
+        try {
+            //조회
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            //JPQL (Member 객체를 대상으로 한다. (대상이 테이블이 아니다))
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
