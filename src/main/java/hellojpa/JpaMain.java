5090a10360a0cecf72a48ff928308374f87b2ee6
Song0-0 <ks08094@naver.com>
2024-01-05 15:51:08 +0900

    플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,24 +15,11 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
-            //비영속 상태
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-
-            //영속 상태 : entityManager안에 있는 영속성 컨텍스트를 통해서 member가 관리가 되는 상태이다.
-
-//            em.persist(member);
-            //회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
-//            em.detach(member);
-
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
-
-            //영속 엔티티의 동일성 보장
-            System.out.println("result = " + (findMember1 == findMember2));
-
+            em.flush();
+            System.out.println("==========================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
